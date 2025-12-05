LMODroid/platform_packages_apps_Launcher3

Launcher3: Switch to search widget provider for hotseat QSB
We don't need to create our own custom QSB view, Google app already
provides search widget which QsbContainerView automatically binds to.

Change-Id: I8c1beb227b6417d606e4229bdb6222ad74dd5d73

src/com/android/launcher3/qsb/QsbContainerView.java
@@ -285,7 +285,7 @@ public class QsbContainerView extends FrameLayout {
         }
 
         public boolean isQsbEnabled() {
-            return FeatureFlags.QSB_ON_FIRST_SCREEN;
+            return Utilities.showQSB(getContext());
         }
 
         protected Bundle createBindOptions() {

gotenksIN/android_packages_apps_Launcher3

Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
Signed-off-by: Saalim Quadri <danascape@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

src/com/android/launcher3/qsb/QsbContainerView.java
@@ -291,8 +291,7 @@ public class QsbContainerView extends FrameLayout {
         }
 
         public boolean isQsbEnabled() {
-            return FeatureFlags.QSB_ON_FIRST_SCREEN
-                    && !SHOULD_SHOW_FIRST_PAGE_WIDGET;
+            return Utilities.isQSBEnabled(getContext());
         }
 
         protected Bundle createBindOptions() {